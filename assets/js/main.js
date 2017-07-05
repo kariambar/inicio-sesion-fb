49e9ec782cc905a0a0c3b81a3ccc73ccf7b0a725
kariambar <karina.spalacios@gmail.com>
2017-07-05 11:55:13 -0400

    subiendo boton

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '148922782339638',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();   
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  //3. Check Login Status
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+//4.Esta es la devolución de llamada. Llama a FB.getLoginStatus()
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
