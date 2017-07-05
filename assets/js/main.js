231e94c5fc799ac6f62b6b96ea55c590264a2472
kariambar <karina.spalacios@gmail.com>
2017-07-05 12:10:11 -0400

    funcion boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,13 @@ window.fbAsyncInit = function() {
     FB.AppEvents.logPageView();   
   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=148922782339638";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));;
 
   //3. Check Login Status
 FB.getLoginStatus(function(response) {
@@ -27,3 +27,4 @@ function checkLoginState() {
     statusChangeCallback(response);
   });
 }
+
